f640353c38434718759a170fbc7e997fc69084cb
ChristianRosales <christian.rosales9029@gmail.com>
2021-05-26 18:47:12 -0500

    mensaje

---
+++ index.js
@@ -0,0 +1,12 @@
+let suma = require('./suma')
+let resta = require('./resta')
+/*
+module.exports = {
+    suma,
+    resta,
+}
+*/
+console.log(suma,(6,11))
+console.log(resta,(10,44))
+let os = require('os')
+console.log(os.cpus())
